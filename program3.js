5c9f6ac6202b5087cf70bf3b0944e50917b0a3de
Masudur Rahman <150067153+Masudur-Rahmanbd@users.noreply.github.com>
2024-11-08 19:16:19 +0600

    Add files via upload

---
+++ program3.js
@@ -0,0 +1,19 @@
+// digit number , swicth
+
+var digit = prompt("enter a digit :");
+switch(digit){
+    case "0":
+        document.write("zero");
+        break;
+        case "1":
+            document.write("one");
+            break;
+            case "2":
+                document.write("two");
+                break;
+                case "3":
+                    document.write("three");
+                    default:
+                        document.write("not a digit");
+        
+}
