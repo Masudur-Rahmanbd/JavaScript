3e13e2d96014385586b10edc37964c668f44f4a5
Masudur Rahman <150067153+Masudur-Rahmanbd@users.noreply.github.com>
2024-11-08 19:17:54 +0600

    Add files via upload

---
+++ program4.js
@@ -0,0 +1,25 @@
+//function 
+
+//addition
+function addition(x,y)
+{
+    var result =x + y;
+    document.write("sum = " + result + "</br>");
+}
+
+
+// subtraction
+ function subtraction(x,y){
+    var result= x - y;
+    document.write("sub = " + result + "</br>");
+ }
+
+ // square
+ function square(x,y){
+    var result= x * y;
+    document.write("square = " +result +"</br");
+
+ }
+  subtraction(10,3);
+  addition(3,5);
+  square(4,6);
