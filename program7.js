8b706348985802e7c7b9e89cf918c4cb2362d8e3
Masudur Rahman <150067153+Masudur-Rahmanbd@users.noreply.github.com>
2024-11-10 23:14:43 +0600

    Add files via upload

---
+++ program7.js
@@ -0,0 +1,15 @@
+var num = new Array();
+
+    for( var i = 0; i < 5; i++){
+       num[i] = parseInt(prompt("enter a number:"));
+    }
+
+
+
+var num = [10, 20, 30, 40, 50];
+var sum = 0;
+for (var i = 0; i < 5; i++) {
+    console.log(num[i]);
+    sum = sum + num [i];
+}
+console.log(" sum = " + sum);
